b7550412a4dfb5bd37711561961832e17cc3d5aa
Gonzalo Paz Delgado <gonzalopd96@uma.es>
2019-09-09 15:33:16 +0200

    First commit on new branch

---
+++ src/CoRa_CostUpdating.cpp
@@ -0,0 +1,18 @@
+/*********************************CoRa******************************************
+                    -Cost ratios updating after traverse-
+                        ARES (ESA-UMA Collaboration)
+                University of Malaga - European Space Agency
+                                -Author-
+                          Gonzalo J. Paz Delgado
+                             -Contact mail-
+                            gonzalopd96@uma.es
+                              -Supervisors-
+                             Martin Azkarate
+                        J. Ricardo Sanchez Ibanez
+                        Carlos J. Perez del Pulgar
+*******************************************************************************/
+
+#include "DyMu.hpp"    // Maybe it is better to create a new header CoRa.hpp
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
